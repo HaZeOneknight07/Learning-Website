3096035c954669f3cc8d4310dc597eebe81f2772
HaZeOneknight07 <159442717+HaZeOneknight07@users.noreply.github.com>
2024-04-07 16:39:07 +0100

    Delete Learning Website directory

--- Learning Website/preloader.js
+++
@@ -1,5 +0,0 @@
-// Delay the hiding of preloader and showing login content after 5 seconds
-setTimeout(function () {
-    document.querySelector('.preloader').classList.add('hide');
-    document.getElementById('main-content').classList.remove('hide');
-}, 5000);
